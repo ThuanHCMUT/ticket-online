47e8971a9238a0675e4047b64a124991f4c520c6
ThuanHCMUT <tieuho036@gmail.com>
2021-11-26 19:21:50 +0700

    update

---
+++ src/pages/admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './adminpage.hbs';
+import './adminpageStyle/app.css';
+import './adminpageStyle/app2.css';
+
+export default class Admin extends Component {
+    render() {
+        return (
+            <>
+                
+            </>
+        );
+    }
+}
